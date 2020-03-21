82c729f92a2999c4345de6a60e19fdf2bb64dcd0
FlyingMug-dev <62445015+FlyingMug-dev@users.noreply.github.com>
2020-03-21 14:13:17 +0000

    Update mac_changer.sh

--- mac_changer.sh
+++ mac_changer.sh
@@ -1,6 +1,7 @@
 echo Which interface do you want to change the MAC address of?
+
 read interface
 
 ifconfig $interface down
-ifconfig $interface hw ether 00:11:22:33:44:55
+ifconfig $interface hw ether [The new MAC address] #change this to the new MAC address 
 ifconfig $interface up
